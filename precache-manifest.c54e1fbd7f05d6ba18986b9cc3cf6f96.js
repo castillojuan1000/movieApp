54bb3b7aad765bd045b62e1469deca8cd7097ba2
Juan Castillo <castillojuan1000@gmail.com>
2020-03-01 19:34:52 -0500

    Updates

---
+++ precache-manifest.c54e1fbd7f05d6ba18986b9cc3cf6f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b4e136867bde1bc5b71a4ca35505b0",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "eb6ae5854bba1269fe53",
+    "url": "/movieApp/static/css/main.82d66f6e.chunk.css"
+  },
+  {
+    "revision": "204836789df6f2bb9983",
+    "url": "/movieApp/static/js/2.b07de0ea.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/movieApp/static/js/2.b07de0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6ae5854bba1269fe53",
+    "url": "/movieApp/static/js/main.a682666a.chunk.js"
+  },
+  {
+    "revision": "e359d9e789c3b8bec54c",
+    "url": "/movieApp/static/js/runtime-main.8ac231de.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/castillojuan1000/movieApp/precache-manifest.efaab76a1570e0e7fbf9d2cd730d6418.js"
+  "/movieApp/precache-manifest.c54e1fbd7f05d6ba18986b9cc3cf6f96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/castillojuan1000/movieApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movieApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
